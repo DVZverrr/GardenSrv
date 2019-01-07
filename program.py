897e31e3dc64aa42da4d20ea1685195cc38ad34e
Dmitry Zverev <dvzverrr@gmail.com>
2019-01-08 03:11:24 +0500

    Trying use sensors from app - fail

--- program.py
+++ program.py
@@ -1,12 +1,21 @@
 from flask import Flask
+from time import sleep
 import Adafruit_DHT
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    hum, temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
-    if hum is not None and temp is not None:
-      return 'Humidity: ' + hum + ' Temp: ' + temp
-    else:
-      return 'Failed to get data from sensor'
+    result = '<html><head></head><body>'
+    for i  in range(50):
+      hum, temp = Adafruit_DHT.read(Adafruit_DHT.AM2302, 17)
+      result = result + 'Attempt {0:d}: '.format(i+1)
+      if hum is not None and temp is not None:
+        result = result + 'Humidity: {0:0.1f}%  Temp: {1:0.1f}C<br>'.format(hum,temp)
+      else:
+        result = result + 'Failed to get data from sensor<br>'
+      sleep(2)
+    return result + '</body></html>'
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=5000, debug=True)
